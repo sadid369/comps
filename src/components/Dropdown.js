67d64a97ea5dd0b536e88c3e424317e42cbe942b
Sadid <sadid.jones@gmail.com>
2023-02-14 22:05:15 +0600

    @14/02/2023 @ start 13 folder

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,24 +1,54 @@
-import { useState } from "react";
-function Dropdown({ options }) {
+import { GoChevronDown } from "react-icons/go";
+import { useState, useEffect, useRef } from "react";
+import Panel from "./Panel";
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handler, true);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
   const hanleOptionClick = (option) => {
     setIsOpen(false);
-    console.log(option);
+    onChange(option);
   };
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => hanleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => hanleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
+
   return (
-    <div>
-      <div onClick={handleClick}>Select...</div>
-      {isOpen && <div> {renderedOptions}</div>}
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full ">{renderedOptions}</Panel>
+      )}
     </div>
   );
 }
